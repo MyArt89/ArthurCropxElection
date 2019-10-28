49d283dc7720157ae6d245bd705052ecd44cde06
arthurGoihberg <arthurgoihberg@gamil.com>
2019-10-28 23:51:14 +0200

    idea fix for intellij

--- election-server/src/main/java/cropx/election/controller/ElectionController.java
+++ election-server/src/main/java/cropx/election/controller/ElectionController.java
@@ -1,7 +1,6 @@
 package cropx.election.controller;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
